2dbeb0786ce8a53f7bb99f0a34038a25d5393573
Marcin199028 <39196926+Marcin199028@users.noreply.github.com>
2018-05-13 13:51:09 +0200

    Update Init.java

--- src/test/java/pl/lait/przychodnia3/Init.java
+++ src/test/java/pl/lait/przychodnia3/Init.java
@@ -31,7 +31,7 @@ public class Init {
 			
 			URL seleniumAdress = null;
 			try {
-				seleniumAdress = new URL("http://192.168.0.116:4444/wd/hub");
+				seleniumAdress = new URL("http://192.168.0.113:4444/wd/hub");
 			} catch (MalformedURLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
